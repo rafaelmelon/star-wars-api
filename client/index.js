44d20906ea5ad7e6f54ac39caeba6ce94e934fa9
Rafael Melón <info@rafaelmelon.es>
2017-08-25 19:06:38 +0200

    First react component - hello world

--- client/index.js
+++ client/index.js
@@ -1,1 +1,5 @@
-console.log('This is a test')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App.jsx'
+
+ReactDOM.render(<App />, document.getElementById('root'))
