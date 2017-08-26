c1b37049a9313094841a5c8819ce6583655e6d6f
Rafael Melón <info@rafaelmelon.es>
2017-08-26 19:31:42 +0200

    Carousel slider - Fetch people

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-function Carousel () {
-  return (
-    <div>
-      <h1>Carousel</h1>
-    </div>
-  )
+import Slider from 'react-slick'
+import styles from './carousel.css'
+
+class Carousel extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { loading: false, people: [] }
+  }
+
+  componentWillMount () {
+    const URL = 'https://swapi.co/api/people/?format=json'
+    fetch(URL).then(res => res.json())
+    .then(data => {
+      let peopleArray = []
+      data.results.forEach(item => {
+        peopleArray.push(item.name)
+      })
+      this.setState({ loading: false, people: peopleArray })
+    })
+    .catch((error) => console.log(error))
+  }
+
+  render () {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+    const peopleList = this.state.people.map(function (slide) {
+      return <div key={slide}><h3>{slide}</h3></div>
+    })
+
+    return (
+      <div className={ styles.root }>
+        { this.state.people.length > 0 ? <Slider {...settings}>{ peopleList }</Slider> : null }
+      </div>
+    )
+  }
+
 }
 
 export default Carousel
